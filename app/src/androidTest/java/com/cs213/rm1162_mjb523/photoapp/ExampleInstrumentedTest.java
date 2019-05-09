fd3484bf377175ae9268a227ad7e7379aac37f8f
Rohit Mahankali <rohit.mahankali.23@gmail.com>
2019-05-08 20:37:51 -0400

    slight tweaks

--- app/src/androidTest/java/com/cs213/rm1162_mjb523/photoapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/cs213/rm1162_mjb523/photoapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.cs213.bhd20_arn55.photoapp", appContext.getPackageName());
+        assertEquals("com.cs213.rm1162_mjb523.photoapp", appContext.getPackageName());
     }
 }
